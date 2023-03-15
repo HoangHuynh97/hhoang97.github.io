c4963258f4d080d53a52fba7ce98f6790865bbdc
HoangHuynh97 <127948690+HoangHuynh97@users.noreply.github.com>
2023-03-15 18:30:41 +0700

    Add files via upload

---
+++ app/Views/home/index.php
@@ -0,0 +1,504 @@
+<?php echo loadHeader(); ?>
+<body>
+	<div class="landing-container">
+		<section class="content-menu">
+			<div class="move-bg"></div>
+			<div class="logo-icon">
+				<span>LOGO</span>
+			</div>
+			<div class="menu">
+				<div class="box-menu active" data-point="1">
+					<span>HOME</span>
+					<div class="hight-box">
+						<div class="line-box"></div>
+					</div>
+				</div>
+				<div class="box-menu" data-point="2">
+					<span>INTRO</span>
+					<div class="hight-box">
+						<div class="line-box"></div>
+					</div>
+				</div>
+				<div class="box-menu" data-point="3">
+					<span>PROJECT</span>
+					<div class="hight-box">
+						<div class="line-box"></div>
+					</div>
+				</div>
+				<div class="box-menu" data-point="4">
+					<span>BLOG</span>
+					<div class="hight-box">
+						<div class="line-box"></div>
+					</div>
+				</div>
+				<div class="box-menu" data-point="5">
+					<span>ABOUT</span>
+					<div class="hight-box">
+						<div class="line-box"></div>
+					</div>
+				</div>
+				<div class="box-menu" data-point="6">
+					<span>CONTACTS</span>
+					<div class="hight-box">
+						<div class="line-box"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="section-nth-1">
+			<div class="container-nth-1-left">
+				<div class="content-banner">
+					<div class="text-banner">
+						<h4>LANDING PAGE</h4>
+					</div>
+					<div class="content-text-banner">
+						<span>You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.</span>
+					</div>
+				</div>
+			</div>
+			<div class="container-nth-1-right">
+				<div class="slide-nth-1">
+					<div class="container-slide-img">
+						<div class="box-img" data-img="1">
+							<div class="before-img"></div>
+							<img src="public/assets/images/slide1.jpg">
+							<div class="after-img"></div>
+						</div>
+						<div class="box-img" data-img="2">
+							<div class="before-img"></div>
+							<img src="public/assets/images/slide2.jpg">
+							<div class="after-img"></div>
+						</div>
+						<div class="box-img" data-img="3">
+							<div class="before-img"></div>
+							<img src="public/assets/images/slide3.jpg">
+							<div class="after-img"></div>
+						</div>
+						<div class="box-img" data-img="4">
+							<div class="before-img"></div>
+							<img src="public/assets/images/slide4.jpg">
+							<div class="after-img"></div>
+						</div>
+						<div class="box-img" data-img="5">
+							<div class="before-img"></div>
+							<img src="public/assets/images/slide5.jpg">
+							<div class="after-img"></div>
+						</div>
+					</div>
+					<div class="dot-local-content">
+						<div class="dot-themes active"></div>
+						<div class="dot-themes"></div>
+						<div class="dot-themes"></div>
+						<div class="dot-themes"></div>
+						<div class="dot-themes"></div>
+					</div>
+					<div class="box-next-or-prev">
+						<div class="btn-prev">
+							<i class="fa-solid fa-arrow-left"></i>
+						</div>
+						<div class="btn-next">
+							<i class="fa-solid fa-arrow-right"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="section-nth-2">
+			<div class="line-menu" id="line-menu-nth-2"></div>
+			<div class="container-nth-2-left">
+				<div class="slide-nth-2">
+					<div class="action-prev-nth-2">
+						<i class="fa-solid fa-angle-left"></i>
+					</div>
+					<div class="container-slide-img-nth-2">
+						<div class="box-img-nth-2" data-img="1">
+							<img src="public/assets/images/slide1.jpg">
+						</div>
+						<div class="box-img-nth-2" data-img="2">
+							<img src="public/assets/images/slide2.jpg">
+						</div>
+						<div class="box-img-nth-2" data-img="3">
+							<img src="public/assets/images/slide3.jpg">
+						</div>
+						<div class="box-img-nth-2" data-img="4">
+							<img src="public/assets/images/slide4.jpg">
+						</div>
+						<div class="box-img-nth-2" data-img="5">
+							<img src="public/assets/images/slide5.jpg">
+						</div>
+					</div>
+					<div class="action-next-nth-2">
+						<i class="fa-solid fa-angle-right"></i>
+					</div>
+					<div class="container-box-dot-nth-2">
+						<div class="inblock w100px">
+							<div class="dot-local-content-nth-2">
+								<div class="dot-themes-nth-2 active"></div>
+								<div class="dot-themes-nth-2"></div>
+								<div class="dot-themes-nth-2"></div>
+								<div class="dot-themes-nth-2"></div>
+								<div class="dot-themes-nth-2"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container-nth-2-right">
+				<div class="content-banner">
+					<div class="text-banner">
+						<h4>INTRODUCTION</h4>
+					</div>
+					<div class="content-text-banner">
+						<span>You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.</span>
+					</div>
+					<button class="btn-themes">MORE DETAILS</button>
+				</div>
+			</div>
+		</section>
+		<section id="section-nth-3">
+			<div class="line-menu" id="line-menu-nth-3"></div>
+			<div class="container-nth-3">
+				<div class="container-box active">
+					<div class="this-new">NEW</div>
+					<div class="container-bg-box">
+						<div class="content-img-box">
+							<img src="public/assets/images/slide1.jpg">
+						</div>
+						<div class="title-box">
+							<h4>LANDING PAGE</h4>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="content-action-box">
+							<div class="show-details">
+								<span>DETAILS</span>
+								<i class="fa-solid fa-plus"></i>
+							</div>
+							<button class="btn-themes-view">VIEW</button>
+						</div>
+					</div>
+					<div class="bg-box"></div>
+				</div>
+				<div class="container-box active">
+					<div class="this-new">NEW</div>
+					<div class="container-bg-box">
+						<div class="content-img-box">
+							<img src="public/assets/images/slide2.jpg">
+						</div>
+						<div class="title-box">
+							<h4>LANDING PAGE</h4>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="content-action-box">
+							<div class="show-details">
+								<span>DETAILS</span>
+								<i class="fa-solid fa-plus"></i>
+							</div>
+							<button class="btn-themes-view">VIEW</button>
+						</div>
+					</div>
+					<div class="bg-box"></div>
+				</div>
+				<div class="container-box">
+					<div class="this-new">NEW</div>
+					<div class="container-bg-box">
+						<div class="content-img-box">
+							<img src="public/assets/images/slide3.jpg">
+						</div>
+						<div class="title-box">
+							<h4>LANDING PAGE</h4>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="content-action-box">
+							<div class="show-details">
+								<span>DETAILS</span>
+								<i class="fa-solid fa-plus"></i>
+							</div>
+							<button class="btn-themes-view">VIEW</button>
+						</div>
+					</div>
+					<div class="bg-box"></div>
+				</div>
+				<div class="container-box">
+					<div class="this-new">NEW</div>
+					<div class="container-bg-box">
+						<div class="content-img-box">
+							<img src="public/assets/images/slide4.jpg">
+						</div>
+						<div class="title-box">
+							<h4>LANDING PAGE</h4>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="content-action-box">
+							<div class="show-details">
+								<span>DETAILS</span>
+								<i class="fa-solid fa-plus"></i>
+							</div>
+							<button class="btn-themes-view">VIEW</button>
+						</div>
+					</div>
+					<div class="bg-box"></div>
+				</div>
+				<div class="container-box">
+					<div class="this-new">NEW</div>
+					<div class="container-bg-box">
+						<div class="content-img-box">
+							<img src="public/assets/images/slide5.jpg">
+						</div>
+						<div class="title-box">
+							<h4>LANDING PAGE</h4>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="content-action-box">
+							<div class="show-details">
+								<span>DETAILS</span>
+								<i class="fa-solid fa-plus"></i>
+							</div>
+							<button class="btn-themes-view">VIEW</button>
+						</div>
+					</div>
+					<div class="bg-box"></div>
+				</div>
+				<div class="container-box">
+					<div class="this-new">NEW</div>
+					<div class="container-bg-box">
+						<div class="content-img-box">
+							<img src="public/assets/images/slide1.jpg">
+						</div>
+						<div class="title-box">
+							<h4>LANDING PAGE</h4>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="content-action-box">
+							<div class="show-details">
+								<span>DETAILS</span>
+								<i class="fa-solid fa-plus"></i>
+							</div>
+							<button class="btn-themes-view">VIEW</button>
+						</div>
+					</div>
+					<div class="bg-box"></div>
+				</div>
+			</div>
+		</section>
+		<section id="section-nth-4">
+			<div class="line-menu" id="line-menu-nth-4"></div>
+			<div class="container-nth-4">
+				<div class="container-box-nth-4">
+					<div class="content-img-box-nth-4">
+						<img src="public/assets/images/slide1.jpg">
+					</div>
+					<div class="content-box-nth-4">
+						<div class="title-box">
+							<h5>15.03.2023</h5>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="read-more">
+							<span>READ MORE</span>
+							<i class="fa-solid fa-arrow-right"></i>
+						</div>
+					</div>
+					<div class="bg-box-nth-4"></div>
+				</div>
+				<div class="container-box-nth-4">
+					<div class="content-img-box-nth-4">
+						<img src="public/assets/images/slide2.jpg">
+					</div>
+					<div class="content-box-nth-4">
+						<div class="title-box">
+							<h5>15.03.2023</h5>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="read-more">
+							<span>READ MORE</span>
+							<i class="fa-solid fa-arrow-right"></i>
+						</div>
+					</div>
+					<div class="bg-box-nth-4"></div>
+				</div>
+				<div class="container-box-nth-4">
+					<div class="content-img-box-nth-4">
+						<img src="public/assets/images/slide3.jpg">
+					</div>
+					<div class="content-box-nth-4">
+						<div class="title-box">
+							<h5>15.03.2023</h5>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="read-more">
+							<span>READ MORE</span>
+							<i class="fa-solid fa-arrow-right"></i>
+						</div>
+					</div>
+					<div class="bg-box-nth-4"></div>
+				</div>
+				<div class="container-box-nth-4">
+					<div class="content-img-box-nth-4">
+						<img src="public/assets/images/slide4.jpg">
+					</div>
+					<div class="content-box-nth-4">
+						<div class="title-box">
+							<h5>15.03.2023</h5>
+						</div>
+						<div class="text-box">
+							You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.
+						</div>
+						<div class="read-more">
+							<span>READ MORE</span>
+							<i class="fa-solid fa-arrow-right"></i>
+						</div>
+					</div>
+					<div class="bg-box-nth-4"></div>
+				</div>
+			</div>
+		</section>
+	</div>
+</body>
+<script type="text/javascript">
+	$(document).ready(function() {
+		Swal.fire({
+		  	icon: 'warning',
+		  	title: 'Oops...',
+		  	text: 'Chưa có bản responsive, vui lòng xem trên PC/LAPTOP!'
+		})
+		setInterval(() => checkTimeBanner(), 1000);
+		setInterval(() => checkTimeBanner2(), 1000);
+	});	
+	
+	var timeOutBanner = 5;
+	function checkTimeBanner() {
+		timeOutBanner--;
+		if(timeOutBanner == 0) {
+			clickSlide("next");
+		}
+	}
+	var timeOutBanner2 = 5;
+	function checkTimeBanner2() {
+		timeOutBanner2--;
+		if(timeOutBanner2 == 0) {
+			clickSlide2("next");
+		}
+	}
+
+	$(".btn-prev").on('click', function (e) {
+		clickSlide("prev");
+	});
+	$(".btn-next").on('click', function (e) {
+		clickSlide("next");
+	});
+
+	$(".action-prev-nth-2").on('click', function (e) {
+		clickSlide2("prev");
+	});
+	$(".action-next-nth-2").on('click', function (e) {
+		clickSlide2("next");
+	});
+
+	function clickSlide(type) {
+		if(type == "next") {
+			$('.box-img:nth-child(1)').appendTo('.container-slide-img');
+		} else if(type == "prev") {
+			$('.box-img:nth-child(5)').prependTo('.container-slide-img');
+		}
+		var point = $('.box-img:nth-child(2)').attr('data-img');
+		if(point == 1) {
+			point = 5;
+		} else {
+			point = point - 1;
+		}
+		$('.dot-themes').removeClass('active');
+		$('.dot-themes:nth-child('+point+')').addClass('active');
+		timeOutBanner = 5;
+	}
+
+	function clickSlide2(type) {
+		if(type == "next") {
+			$('.box-img-nth-2:nth-child(1)').appendTo('.container-slide-img-nth-2');
+		} else if(type == "prev") {
+			$('.box-img-nth-2:nth-child(5)').prependTo('.container-slide-img-nth-2');
+		}
+		var point = $('.box-img-nth-2:nth-child(2)').attr('data-img');
+		if(point == 1) {
+			point = 5;
+		} else {
+			point = point - 1;
+		}
+		$('.dot-themes-nth-2').removeClass('active');
+		$('.dot-themes-nth-2:nth-child('+point+')').addClass('active');
+		timeOutBanner2 = 5;
+	}
+	
+	let win_height = screen.height - 100;
+	$(function () {
+        var $win = $(window);
+        $win.scroll(function () {
+            if ($win.scrollTop() == 0) {
+                $('.content-menu').removeClass('active');
+                $('.box-menu').removeClass('active');
+				$('.box-menu[data-point="1"]').addClass('active');
+            }
+            else {
+                $('.content-menu').addClass('active');
+            }
+
+            var elem_nth_2 = document.querySelector("#line-menu-nth-2");
+			var point_nth_2 = elem_nth_2.getBoundingClientRect();
+			if(point_nth_2.top <= 250 && point_nth_2.bottom < win_height) {
+				$('.box-menu').removeClass('active');
+				$('.box-menu[data-point="2"]').addClass('active');
+				$('#line-menu-nth-2').addClass('active');
+				$('.container-nth-2-left').addClass('active');
+				$('.container-nth-2-right').addClass('active');
+				
+			} else {
+				$('#line-menu-nth-2').removeClass('active');
+				$('.container-nth-2-left').removeClass('active');
+				$('.container-nth-2-right').removeClass('active');
+			}
+
+			var elem_nth_3 = document.querySelector("#line-menu-nth-3");
+			var point_nth_3 = elem_nth_3.getBoundingClientRect();
+			if(point_nth_3.top <= 250 && point_nth_3.bottom < win_height) {
+				$('.box-menu').removeClass('active');
+				$('.box-menu[data-point="3"]').addClass('active');
+				$('#line-menu-nth-3').addClass('active');
+				$('.container-box').addClass('in');
+				
+			} else {
+				$('#line-menu-nth-3').removeClass('active');
+				$('.container-box').removeClass('in');
+			}
+
+			var elem_nth_4 = document.querySelector("#line-menu-nth-4");
+			var point_nth_4 = elem_nth_4.getBoundingClientRect();
+			if(point_nth_4.top <= 400 && point_nth_4.bottom < win_height) {
+				$('.box-menu').removeClass('active');
+				$('.box-menu[data-point="4"]').addClass('active');
+				$('#line-menu-nth-4').addClass('active');
+				$('.container-nth-4').addClass('active');
+			} else {
+				$('#line-menu-nth-4').removeClass('active');
+				$('.container-nth-4').removeClass('active');
+			}
+        });
+    });
+
+</script>
+<?php echo loadFooter(); ?>
